475bf0f5884e0f83438ea43a5f79ff4f5093dffd
lhenningson <l.henningson@share.epsb.ca>
2022-11-28 11:23:00 -0700

    first

---
+++ function.js
@@ -0,0 +1,96 @@
+// FUNCTIONS
+
+// Draw Start Screen
+function drawStart() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, 200, 250);
+  
+    // Start Text
+    ctx.font = "40px Consolas";
+    ctx.fillStyle = "lightblue";
+    ctx.fillText("CLICK TO START", 350, 285)
+  
+    ctx.font = "25px Consolas";
+    ctx.fillText("CLICK AND HOLD LEFT MOUSE BUTTON TO GO UP", 100, 450);
+    ctx.fillText("RELEASE TO GO DOWN", 415, 480);
+  }
+  
+  // Draw Game Elements
+  function drawGame() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, 200, 250);
+  
+    // Draw Wall 1
+    ctx.fillStyle = "green";
+    ctx.fillRect(700, 200, 50, 100);
+  }
+  
+  // Draw Game Over Screen
+  function drawGameOver() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, 200, 250);
+  
+    // Draw Wall 1
+    ctx.fillStyle = "green";
+    ctx.fillRect(700, 200, 50, 100);
+  
+    // Circle around Helicopter
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(240, 270, 60, 0, 2 * Math.PI);
+    ctx.stroke();
+  
+    // Game Over Text
+    ctx.font = "40px Consolas";
+    ctx.fillStyle = "lightblue";
+    ctx.fillText("GAME OVER", 350, 285);
+  }
